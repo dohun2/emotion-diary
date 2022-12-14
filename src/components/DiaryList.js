d838d313de0cf5bae25b78a0cbb2738101812b5d
이도훈 <qwer2854@naver.com>
2022-12-14 23:18:25 +0900

    Feat: 게시물 시간순 정렬 추가

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -1,7 +1,46 @@
+import { useState } from "react";
+
+const sortOptionList = [
+  { value: "latest", name: "최신순" },
+  { value: "oldest", name: "오래된 순" },
+];
+
+const ControlMenu = ({ value, onChange, optionList }) => {
+  return (
+    <select value={value} onChange={(e) => onChange(e.target.value)}>
+      {optionList.map((e, idx) => (
+        <option key={idx} value={e.value}>
+          {e.name}
+        </option>
+      ))}
+    </select>
+  );
+};
+
 const DiaryList = ({ diaryList }) => {
+  const [sortType, setSortType] = useState("latest");
+
+  const getProcessedDiaryList = () => {
+    const compare = (a, b) => {
+      if (sortType === "latest") {
+        return +b.date - +a.date;
+      } else {
+        return +a.date - +b.date;
+      }
+    };
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const sortedList = copyList.sort(compare);
+    return sortedList;
+  };
+
   return (
     <div>
-      {diaryList.map((e) => (
+      <ControlMenu
+        value={sortType}
+        onChange={setSortType}
+        optionList={sortOptionList}
+      />
+      {getProcessedDiaryList().map((e) => (
         <div key={e.id}>{e.content}</div>
       ))}
     </div>
