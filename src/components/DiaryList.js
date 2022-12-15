02db37f18d2c00c595e277acd4ce9be88e16c164
이도훈 <qwer2854@naver.com>
2022-12-15 11:17:06 +0900

    Feat: 게시물 감정별로 보기 추가

--- src/components/DiaryList.js
+++ src/components/DiaryList.js
@@ -5,6 +5,11 @@ const sortOptionList = [
   { value: "oldest", name: "오래된 순" },
 ];
 
+const filterOptionList = [
+  { value: "all", name: "전부" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안좋은 감정만" },
+];
 const ControlMenu = ({ value, onChange, optionList }) => {
   return (
     <select value={value} onChange={(e) => onChange(e.target.value)}>
@@ -19,8 +24,17 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 
 const DiaryList = ({ diaryList }) => {
   const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
+    const filterCallBack = (e) => {
+      if (filter === "good") {
+        return +e.emotion >= 3;
+      } else {
+        return +e.emotion < 3;
+      }
+    };
+
     const compare = (a, b) => {
       if (sortType === "latest") {
         return +b.date - +a.date;
@@ -29,7 +43,10 @@ const DiaryList = ({ diaryList }) => {
       }
     };
     const copyList = JSON.parse(JSON.stringify(diaryList));
-    const sortedList = copyList.sort(compare);
+
+    const filteredList =
+      filter === "all" ? copyList : copyList.filter((e) => filterCallBack(e));
+    const sortedList = filteredList.sort(compare);
     return sortedList;
   };
 
@@ -40,8 +57,15 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((e) => (
-        <div key={e.id}>{e.content}</div>
+        <div key={e.id}>
+          {e.content} {e.emotion}
+        </div>
       ))}
     </div>
   );
