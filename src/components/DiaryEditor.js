2a08ed86336298b48e90d3b4d9e541b0592f3c48
이도훈 <qwer2854@naver.com>
2022-12-22 23:56:59 +0900

    Feat: localStorage 추가, DiaryEditor 삭제 추가

--- src/components/DiaryEditor.js
+++ src/components/DiaryEditor.js
@@ -15,7 +15,7 @@ const DiaryEditor = ({ isEdit, originData }) => {
   const [emotion, setEmotion] = useState(3);
   const [date, setDate] = useState(getStringDate(new Date()));
 
-  const { onCreate, onEdit } = useContext(DiaryDispatchContext);
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
   const navigate = useNavigate();
 
   const handleClickEmote = (emotion) => {
@@ -42,6 +42,13 @@ const DiaryEditor = ({ isEdit, originData }) => {
     navigate("/", { replace: true });
   };
 
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
   useEffect(() => {
     if (isEdit) {
       setDate(getStringDate(new Date(+originData.date)));
@@ -55,6 +62,13 @@ const DiaryEditor = ({ isEdit, originData }) => {
         headText={isEdit ? "일기 수정하기" : "새 일기쓰기"}
         leftChild={
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          <MyButton
+            text={"삭제하기"}
+            type={"negative"}
+            onClick={handleRemove}
+          />
         }
       />
       <div>
